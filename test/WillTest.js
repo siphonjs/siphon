442720606e7023084a8e4e5f41a214bc964a96c9
Will Galebach <willbach@gmail.com>
2016-11-19 16:39:01 -0800

    added setProxy and overwrite methods, proxy is not working yet (#3)
    * finished get, retries, store, setHeaders methods
    
    * added test files for manual tests
    
    * added methods for setProxy and overwriteData, proxy does not work yet

--- test/WillTest.js
+++ test/WillTest.js
@@ -1,14 +1,19 @@
 const siph = require('./../lib/Siphon');
+const http = require('http');
 
+var counter = 0;
+var proxies = [];
 
 var mySiphon = siph()
 // .get('https://www.wunderground.com/cgi-bin/findweather/getForecast?query=90025')
 .find(/[0-9]{2}\.[0-9]/)
 .store( (datum) => {
-  console.log(datum[0]);
+  console.log(datum);
 })
 
-
-
-.execute();
+for(let i = 90025; i < 90030; i++) {
+  mySiphon.get(`https://www.wunderground.com/cgi-bin/findweather/getForecast?query=${i}`)
+  // .setProxy('http://201.243.152.108:8080')
+  .execute()
+}
 
