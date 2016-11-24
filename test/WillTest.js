3496693fb0a84d27237ae2e19b604f456be422b2
Will Galebach <willbach@gmail.com>
2016-11-23 16:18:31 -0800

    updated cluster to save all data at the master level

--- test/WillTest.js
+++ test/WillTest.js
@@ -1,5 +1,20 @@
-const siph = require('./../lib/Siphon');
+const siphon = require('./../lib/Siphon');
 const http = require('http');
+
+
+// var webdriver = require('selenium-webdriver'),
+//   By = webdriver.By,
+//   until = webdriver.until;
+
+// var driver = new webdriver.Builder()
+//   .forBrowser('chrome')
+//   .build();
+
+// driver.get('http://www.google.com/ncr');
+// driver.findElement(By.name('q')).sendKeys('webdriver');
+// driver.findElement(By.name('btnG')).click();
+// driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+// driver.quit();
 
 var counter = 0;
 var proxies = [];
@@ -9,8 +24,8 @@ for(let i = 90025; i < 90030; i++) {
   temps.push(`https://www.wunderground.com/cgi-bin/findweather/getForecast?query=${i}`);
 }
 
-var mySiphon = siph()
+var mySiphon = siphon()
 .find(/[0-9]{2}\.[0-9]/)
 .store( (datum) => { console.log(datum) })
-.get('https://www.wunderground.com/cgi-bin/findweather/getForecast?query=90025')
+.setURLs(temps)
 .run()
