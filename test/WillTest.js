8374c0ce468369e8dca9ce6ba4515a73db389733
Sooeung Lee <sooeung2@gmail.com>
2016-11-29 15:22:55 -0800

    changes

--- test/WillTest.js
+++ test/WillTest.js
@@ -1,30 +1,37 @@
 const siphon = require('./../lib/Siphon');
-// const http = require('http');
+const http = require('http');
 
 
-// var proxies = [];
-// var jobs = [];
-// var temps = [];
-// for(let i = 90025; i < 90030; i++) {
-//   temps.push(`https://www.wunderground.com/cgi-bin/findweather/getForecast?query=${i}`);
-// }
-//
-// var temps = ['http://jakeaustwick.me/a-scrapers-toolkit-redis/'];
-// var proxs = ['http://52.53.162.98:808'];
+var proxies = [];
+var jobs = [];
+var temps = [];
+for(let i = 90025; i < 92025; i++) {
+  temps.push(`https://www.wunderground.com/cgi-bin/findweather/getForecast?query=${i}`);
+}
+
+var mySiphon = siphon()
+.find(/[0-9]{2}\.[0-9]/)
+.store( (returnMessage) => {
+  console.log(returnMessage);
+  console.log('errors: ', returnMessage.errors.length, 'data: ', returnMessage.data.length);
+})
+.retries(2)
+.setURLs(temps)
+.run()
+
+// driver.get('http://www.wunderground.com');
+// console.log("working");
+// driver.quit();
 //
 // var mySiphon = siphon()
-// .find(/[0-9]{2}\.[0-9]/)
-// .store( (returnMessage) => {
-//   console.log(returnMessage);
-//   console.log('errors: ', returnMessage.errors.length, 'data: ', returnMessage.data.length);
+// .get('http://www.wunderground.com')
+// .selenium('chrome', driver => {
+//   const currentLocation = driver
+//     .findElement({className: 'fi-target-two'})
+//     .click()
+//
+//   return currentLocation;
 // })
-// .retries(2)
-// .find(/Scraper/)
-// .store( (returnMessage) => { console.log(returnMessage) })
-// .setURLs(temps)
-// .setProxies(proxs)
-// .run()
-
-var mySiphon = siphon().selenium();
+// .execute();
 
 //https://www.wunderground.com/cgi-bin/findweather/getForecast?query=98004
