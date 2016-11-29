14668d3f1e8f44163b9f1de49cb051bcaf8c710a
Sooeung Lee <sooeung2@gmail.com>
2016-11-29 10:35:25 -0800

    changes

--- test/WillTest.js
+++ test/WillTest.js
@@ -1,58 +1,30 @@
 const siphon = require('./../lib/Siphon');
-const http = require('http');
-
-var webdriver = require('selenium-webdriver')
-var driver = new webdriver.Builder()
-    .forBrowser('chrome')
-    .build();
-
-driver.get('http://www.google.com/');
-driver.findElement(webdriver.By.name('q')).sendKeys('wiki');
-driver.findElement({ name: 'q'}).sendKeys(webdriver.Key.ENTER);
-// driver.wait(until.titleIs('webdriver - Google Search'), 1000);
-driver.wait(check_title, 1000)
-driver.quit();
-
-function check_title() {
-  var promise = driver.getTitle().then(function(title){
-    if (title === 'wiki - Google Search') {
-      console.log('success');
-    } else {
-      console.log('fail --' + title);
-    }
-  });
-  return promise;
-}
+// const http = require('http');
 
 
-// driver.get('http://www.google.com/ncr');ki
-// driver.findElement(By.name('q')).sendKeys('webdriver');
-// driver.findElement(By.name('btnG')).click();
-// driver.wait(until.titleIs('webdriver - Google Search'), 1000);
-// driver.quit();
+// var proxies = [];
+// var jobs = [];
+// var temps = [];
+// for(let i = 90025; i < 90030; i++) {
+//   temps.push(`https://www.wunderground.com/cgi-bin/findweather/getForecast?query=${i}`);
+// }
+//
+// var temps = ['http://jakeaustwick.me/a-scrapers-toolkit-redis/'];
+// var proxs = ['http://52.53.162.98:808'];
+//
+// var mySiphon = siphon()
+// .find(/[0-9]{2}\.[0-9]/)
+// .store( (returnMessage) => {
+//   console.log(returnMessage);
+//   console.log('errors: ', returnMessage.errors.length, 'data: ', returnMessage.data.length);
+// })
+// .retries(2)
+// .find(/Scraper/)
+// .store( (returnMessage) => { console.log(returnMessage) })
+// .setURLs(temps)
+// .setProxies(proxs)
+// .run()
 
-var proxies = [];
-var jobs = [];
-var temps = [];
-for(let i = 90025; i < 90030; i++) {
-  temps.push(`https://www.wunderground.com/cgi-bin/findweather/getForecast?query=${i}`);
-}
-
-var temps = ['http://jakeaustwick.me/a-scrapers-toolkit-redis/'];
-var proxs = ['http://52.53.162.98:808'];
-
-var mySiphon = siphon()
-.find(/[0-9]{2}\.[0-9]/)
-.store( (returnMessage) => {
-  console.log(returnMessage);
-  console.log('errors: ', returnMessage.errors.length, 'data: ', returnMessage.data.length);
-})
-.retries(2)
-.find(/Scraper/)
-.store( (returnMessage) => { console.log(returnMessage) })
-.setURLs(temps)
-.setProxies(proxs)
-.run()
-dfdsjfkdsl
+var mySiphon = siphon().selenium();
 
 //https://www.wunderground.com/cgi-bin/findweather/getForecast?query=98004
