1f9a13ee268e2d8bfe227d39486ecd85ff300822
Sooeung Lee <sooeung2@gmail.com>
2016-12-01 12:18:05 -0800

    changes

--- test/WillTest.js
+++ test/WillTest.js
@@ -8,16 +8,10 @@ for(let i = 90025; i < 91025; i++) {
 }
 
 var regularSiphon = siphon()
-.setURLs(temps)
-.find(/[0-9]{2}\.[0-9]/)
-.retries(2)
-.store( (data) => {
-  // console.log(data);
-})
-// .selenium('chrome', (driver) => {
-//   data = driver.findElement({className: 'city-nav-header'}).getText()
-//   driver.quit();
-//   return data;
-// })
-// .setInterval(2000)
-.run()
+  .setURLs(temps)
+  .find(/[0-9]{2}\.[0-9]/)
+  .retries(2)
+  .store( (data) => {
+    // console.log(data);
+  })
+  .run()
