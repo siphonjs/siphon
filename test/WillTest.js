ce8848df706857b0a62ad799142a019e17ebbc34
Sooeung Lee <sooeung2@gmail.com>
2016-12-03 11:00:26 -0800

    changes

--- test/WillTest.js
+++ test/WillTest.js
@@ -1,17 +1,12 @@
 const siphon = require('./../lib/Siphon');
-const http = require('http');
 
-var temps = [];
-
-for(let i = 90025; i < 91025; i++) {
-  temps.push(`https://www.wunderground.com/cgi-bin/findweather/getForecast?query=${i}`);
-}
 
 var regularSiphon = siphon()
-  .setURLs(temps)
   .find(/[0-9]{2}\.[0-9]/)
   .retries(2)
   .store( (data) => {
     // console.log(data);
   })
-  .run()
+  .setRedis('6379', '138.68.48.182', 'siphontestingnodejsforcodesmith')
+  .dequeueAndExecute()
+  
