79a74f88cf3a08dc9d690d0a75fa8094d01440ff
George Norberg <george.norberg@gmail.com>
2016-12-05 18:00:15 -0800

    modified slightly

--- test/WillTest.js
+++ test/WillTest.js
@@ -4,9 +4,10 @@ const siphon = require('./../lib/Siphon');
 var regularSiphon = siphon()
   .find(/[0-9]{2}\.[0-9]/)
   .retries(2)
-  .store( (data) => {
+  .store((data) => {
     // console.log(data);
   })
+  .notify(statusMessage => console.log(statusMessage))
   .setRedis('6379', '138.68.48.182', 'siphontestingnodejsforcodesmith')
   .dequeueAndExecute()
   
