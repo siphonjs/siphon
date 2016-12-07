5c5821db238fbb6bbf39bf098b56d6b0752c87b0
Will Galebach <willbach@gmail.com>
2016-12-06 18:51:00 -0800

    removed vars in run method

--- test/WillTest.js
+++ test/WillTest.js
@@ -1,13 +1,3 @@
-const siphon = require('./../lib/Siphon');
 
 
-var regularSiphon = siphon()
-  .find(/[0-9]{2}\.[0-9]/)
-  .retries(2)
-  .store((data) => {
-    // console.log(data);
-  })
-  .notify(statusMessage => console.log(statusMessage))
-  .setRedis('6379', '138.68.48.182', 'siphontestingnodejsforcodesmith')
-  .dequeueAndExecute()
-  
+
